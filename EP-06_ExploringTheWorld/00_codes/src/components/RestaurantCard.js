54e8b98ae438fceb60cc03a3ba76ad41cc6cdeae
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-04-24 23:13:04 +0530

    Source code of EP-06 🚀

---
+++ EP-06_ExploringTheWorld/00_codes/src/components/RestaurantCard.js
@@ -0,0 +1,35 @@
+import { CDN_URL } from "../utils/constants";
+
+// RestaurantCards components and props
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    locality,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+
+  return (
+    <div className="resCards" style={{ background: "#f0f0f0" }}>
+      <img
+        className="img-resCard"
+        alt="res-logo"
+        src={CDN_URL + resData.data.cloudinaryImageId}
+      />
+
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{locality}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} mins</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
---
+++ EP-06_ExploringTheWorld/00_codes/src/utils/constants.js
@@ -0,0 +1,9 @@
+// Named export
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://penji.co/wp-content/uploads/2022/08/11.Foodigy-logo.jpg";
+
+export const CART_URL =
+  "https://img.freepik.com/free-photo/minimal-shopping-cart-shopping-concept-orange-background-3d-rendering_56104-1396.jpg?t=st=1713776529~exp=1713780129~hmac=98dc42db70377b9c466ed6524c9d413bb9d4239390308743c93699a77e7f9cb5&w=1060";
