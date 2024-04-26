df6e0d1f6bccc37f3be7ec14769a5c18f988549f
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-04-26 11:57:34 +0530

    add Ep=07 part 05

---
+++ EP-07_FindingThePath/00_codes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="">
+      <h1>Taniya</h1>
+      <h2>This is namaste-react web series</h2>
+    </div>
+  );
+};
+
+export default About;
